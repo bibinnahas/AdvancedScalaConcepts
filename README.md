89ad906b1c156341556e4fdd1edb528da2c0b5a1
Bibin Nahas <bbnnaz@gmail.com>
2021-05-04 17:42:12 +0530

    README updated

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ This project is my attempt to go back to the basics and dig further into the fun
 ## Courses referred for this.
 - Rock The JVM courses by Daniel
 - Random blogs from functional programmers
-
+## Index
+- Advanced - Include scala concepts which is bit more advanced. It might not be advanced to you, however, I just put them here because I wasn't really using these much in my programs.
+- Basic - This includes very basic concepts. Time to time a refresh helps for a giant leap.
--- src/main/scala/com/bbn/fp/basic/Functions.scala
+++ src/main/scala/com/bbn/fp/basic/Functions.scala
@@ -7,21 +7,57 @@ object Functions {
   /**
    * A simple function to concatenate string to the number of times
    * using tail recurssion
+   *
    * @param word
    * @param times
    * @return words * times
    */
-  def stringConcatenator(word: String, times: Int): String = {
+  def stringConcatenator(word: String, times: Int = 5): String = {
     @tailrec
     def helper(word: String, times: Int, accu: String): String = {
       if (times == 0) accu
       else helper(accu, times - 1, word + accu)
     }
+
     helper(word, times, "")
   }
 
-  def main(args: Array[String]): Unit = {
-    println(stringConcatenator("Hello ", 5))
+  /**
+   * Simple function to find if a number is prime or not
+   *
+   * @param n
+   * @return
+   */
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeUntil(t: Int): Boolean = {
+      if (t <= 1) true
+      else (n % t) != 0 && isPrimeUntil(t - 1)
+    }
+
+    isPrimeUntil(n / 2)
   }
 
-}
+  /**
+   * Difference between call by name and call by value
+   *
+   * @param x
+   */
+  def callByName(x: => Long) = {
+    println(s"by name: $x")
+    println(s"by name: $x")
+  }
+
+  def callByValue(x: Long) = {
+    println(s"by value: $x")
+    println(s"by value: $x")
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(stringConcatenator("Hello ", 7))
+    println(isPrime(7))
+    //    println(callByName(System.nanoTime()))
+    //    println(callByValue(System.nanoTime()))
+  }
+
+} 
