74248bdab595eaa5794ea958968cac83d1ab20f3
Bibin Nahas <bbnnaz@gmail.com>
2021-05-06 16:31:44 +0530

    Generalised Custom List modified

--- src/main/scala/com/bbn/fp/basic/GenericMyList.scala
+++ src/main/scala/com/bbn/fp/basic/GenericMyList.scala
@@ -13,6 +13,14 @@ abstract class GenericMyList[+A] {
 
   override def toString: String = "[" + printElements + "]"
 
+  def map[B](transformer: MyTransformer[A, B]): GenericMyList[B]
+
+  def flatMap[B](transformer: MyTransformer[A, GenericMyList[B]]): GenericMyList[B]
+
+  def filter(predicate: MyPredicate[A]): GenericMyList[A]
+
+  def ++[B >: A](lst: GenericMyList[B]): GenericMyList[B]
+
 }
 
 object Empty extends GenericMyList[Nothing] {
@@ -25,6 +33,14 @@ object Empty extends GenericMyList[Nothing] {
   def add[B >: Nothing](elem: B): GenericMyList[B] = new Cons(elem, Empty)
 
   def printElements: String = ""
+
+  def map[B](transformer: MyTransformer[Nothing, B]): GenericMyList[B] = Empty
+
+  def flatMap[B](transformer: MyTransformer[Nothing, GenericMyList[B]]): GenericMyList[B] = Empty
+
+  def filter(predicate: MyPredicate[Nothing]): GenericMyList[Nothing] = Empty
+
+  def ++[B >: Nothing](lst: GenericMyList[B]): GenericMyList[B] = lst
 }
 
 class Cons[+A](h: A, t: GenericMyList[A]) extends GenericMyList[A] {
@@ -34,13 +50,33 @@ class Cons[+A](h: A, t: GenericMyList[A]) extends GenericMyList[A] {
 
   def isEmpty: Boolean = false
 
-  def add[B >: A](elem: B): GenericMyList[B] = new Cons(elem, new Cons(h, t))
+  def add[B >: A](elem: B): GenericMyList[B] = new Cons(elem, this)
 
   def printElements: String = {
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
 
   }
+
+  def map[B](transformer: MyTransformer[A, B]): GenericMyList[B] =
+    new Cons(transformer.transform(h), t.map(transformer))
+
+  def flatMap[B](transformer: MyTransformer[A, GenericMyList[B]]): GenericMyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+
+  def filter(predicate: MyPredicate[A]): GenericMyList[A] =
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+  def ++[B >: A](lst: GenericMyList[B]): GenericMyList[B] = new Cons(h, t ++ lst)
+}
+
+trait MyPredicate[-T] {
+  def test(elem: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(elem: A): B
 }
 
 object ListTest extends App {
@@ -51,5 +87,16 @@ object ListTest extends App {
   println(listOfIntegers.head)
   println(listOfStrings.head)
   println(listOfStrings.add("kunjan").head)
+  println(listOfIntegers.map(new MyTransformer[Int, Int] {
+    override def transform(elem: Int): Int = elem * 2
+  }))
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(elem: Int): Boolean = elem % 2 == 0
+  }))
+  println(listOfIntegers ++ listOfStrings)
+  println(listOfIntegers.flatMap(new MyTransformer[Int, GenericMyList[Int]] {
+    override def transform(elem: Int): GenericMyList[Int] = new Cons(elem, new Cons(elem + 1, Empty))
+  }))
+
 }
 
